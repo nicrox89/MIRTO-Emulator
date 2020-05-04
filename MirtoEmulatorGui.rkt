17b2ef5ef17fd819a32e457ee46f254aaca6d76d
Nicola Russo <russo.nik@gmail.com>
2020-05-04 21:16:45 +0200

    Add custom button and slider

--- MirtoEmulatorGui.rkt
+++ MirtoEmulatorGui.rkt
@@ -32,16 +32,19 @@
          )
 
 
-;(require math/matrix)
-;(require 2htdp/image)
-;(require images/flomap)
 (require picturing-programs)
 
+;GUI Threads
 (define gui-thread null)
 
-(define mouse_x 0)
-(define mouse_y 0)
+;Mouse position - for bot position
+(define mouse-x 0)
+(define mouse-y 0)
 
+;Initial potentiometer position
+(define slider-x 30)
+
+;distance with the centroid
 (define bumpDelta 18)
 
 ;initial position and direction in radiants
@@ -53,12 +56,12 @@
 (define cosz 0)
 (define sinz 0)
 
-;variables for power 
-(define delta 0)
-(define power 0)
+;power
+(define delta 0) ;balance left-right power
+(define power 0) ;global direction power
 
-(define rightWheelPwr 0)
-(define leftWheelPwr 0)
+(define rightWheelPwr 0) ;right motor power
+(define leftWheelPwr 0) ;left motor power
 
 ;bumpers
 (define bumpersInterval 0) ;0 means disabled
@@ -122,7 +125,7 @@
 (define blacks (indexes-of-black simple_list))
 
 
-
+;; Main function that compute the bot position
 (define (position) 
   (set! delta (* 0.0001 (- rightWheelPwr leftWheelPwr)))
   (set! power (* 0.01 (/ ( + leftWheelPwr rightWheelPwr) 2)))
@@ -195,13 +198,16 @@
   
 )
                        
+;; *******************************
+;; ********** Windowing **********
+;; *******************************
 
-;windowing
+;;Bot frame
 (define frame (let ([new-es (make-eventspace)])
                 (parameterize ([current-eventspace new-es])
                   (new
                    (class frame%
-                     (super-new [label "Frame"]
+                     (super-new [label "MIRTO Emulator"]
                                 [style '(no-resize-border)]
                                 [width (+ WIDTH TOOLSWIDTH)]
                                 [height HEIGHT]
@@ -212,6 +218,8 @@
                   ))
   )
 
+
+;;Panel container
 (define mainPanel (new horizontal-panel%
                    [parent frame]
                    [min-width (+ WIDTH TOOLSWIDTH)]	 
@@ -219,6 +227,7 @@
                    )
   )
 
+;;Bot moving area
 (define leftPanel (new panel%
                    [parent mainPanel]
                    [min-width WIDTH]	 
@@ -226,6 +235,7 @@
                    )
   )
 
+;;Right panel container
 (define rightPanel (new vertical-panel%
                    [parent mainPanel]
                    [min-width TOOLSWIDTH]	 
@@ -233,6 +243,7 @@
                    )
   )
 
+;;Controls panel
 (define topRightPanel
   (new vertical-panel%
        [parent rightPanel]
@@ -241,6 +252,7 @@
        )
   )
 
+;;Display panel
 (define bottomRightPanel (new vertical-panel%
                    [parent rightPanel]
                    [min-width TOOLSWIDTH]	 
@@ -248,22 +260,111 @@
                    )
   )
 
-(define slider (new slider% [label ""]
-                            [min-value 0]
-                            [max-value 1023]
-                            [parent topRightPanel]
-                            [init-value 0]
-                            [style '(horizontal)]
-                            [vert-margin 10]
-                            [horiz-margin 10]))
+;; ************* TEST NEW POTENTIOMETER *****************
+
+;;Potentiometer
+;(define slider (new slider% [label ""]
+;                            [min-value 0]
+;                            [max-value 1023]
+;                            [parent topRightPanel]
+;                            [init-value 0]
+;                            [style '(horizontal)]
+;                            [vert-margin 10]
+;                            [horiz-margin 10]))
 
 
-(define onboardButton (new button% [parent topRightPanel]
-                                   [label "onboard button"]
-                                   [callback (λ (c dc) (set! button #t))]))
+;;Onboard button
+;(define onboardButton (new button% [parent topRightPanel]
+;                                   [label "onboard button"]
+;                                   [callback (λ (c dc) (set! button #t))]))
+
+(define slider-cb (lambda (e)
+                    (define x (send e get-x))
+                    (cond [ (and (> x 30) (< x 170))
+                            (set! slider-x x)
+                            (set! potentiometer (exact-round (/ (* (- x 29) 1023) 141)))
+                           ]
+                          [ (< x 31)
+                            (set! slider-x 30)
+                            (set! potentiometer 0)
+                            ]
+                          [ (> x 169)
+                            (set! slider-x 170)
+                            (set! potentiometer 1023)]
+                          )))
+
+(define canvas-slider%
+  (class canvas%
+    (define/override (on-event e)
+           (when (equal? (send e dragging?) #t)
+             (slider-cb e)
+             ))
+    (super-new)))
+
+;;Slider
+(define sliderEmulator (new canvas-slider%
+                 [parent topRightPanel]
+                 [paint-callback
+                  (λ (c dc)
+                    (send dc erase)
+                    (send dc set-pen "black" 2 'solid)
+                    (send dc draw-rounded-rectangle 30 100 140 6 2)
+                    (send dc set-pen "orange" 3 'solid)
+                    (send dc draw-line 32 102 slider-x 102)
+                    (send dc set-pen "blue" 10 'solid)
+                    (send dc draw-point slider-x 102)
+                    
+                    )
+                  
+                  ]
+                 [style '(transparent)]
+                 )
+  )
 
 
-;;display
+
+;; ************* TEST NEW BUTTON *****************
+
+(define button-cb (λ () (cond [ (equal? button #t) (set! button #f)]
+                              [ (equal? button #f) (set! button #t)])))
+
+(define canvas-button%
+  (class canvas%
+    (define/override (on-event e)
+           (when (equal? (send e get-event-type) 'left-down)
+             (button-cb)
+             ))
+    (super-new)))
+
+;;Button
+(define buttonEmulator (new canvas-button%
+                 [parent topRightPanel]
+                 [paint-callback
+                  (λ (c dc)
+                    (send dc erase)
+                    (cond [ (equal? button #t)
+                            (send dc set-pen "blue" 5 'solid)
+                            (send dc set-brush "blue" 'solid)
+                            ]
+                          [ (equal? button #f)
+                            (send dc set-pen "red" 5 'solid)
+                            (send dc set-brush "red" 'solid)
+                            ])
+                    (send dc draw-rounded-rectangle 30 50 140 30)
+                    (send dc set-font (make-font #:size 18 #:family 'roman #:weight 'bold))
+                    (send dc set-text-foreground "white")
+                    (send dc draw-text "onboard button" 40 55)
+                    )
+                  
+                  ]
+                 [style '(transparent)]
+                 )
+  )
+
+
+
+
+;;Bot Display
 (define display (new canvas%
                  [parent bottomRightPanel]
                  [paint-callback
@@ -278,43 +379,40 @@
                     (send dc set-pen "blue" 3 'solid)
                     (send dc set-brush "blue" 'solid)
                     (send dc draw-rectangle
-                          5 150   ; Top-left at (0, 10), 10 pixels down from top-left
-                          190 90) ; 30 pixels wide and 10 pixels high
+                          5 150   ; Top-left at (##, ##) inside the panel
+                          190 90) ; ## pixels wide and ## pixels high
                     
                     (send dc set-pen "white" 1 'solid)
                     (send dc draw-rectangle
-                          8 153   ; Top-left at (0, 10), 10 pixels down from top-left
-                          183 84) ; 30 pixels wide and 10 pixels high
+                          8 153   ; Top-left at (##, ##) inside the panel
+                          183 84) ; ## pixels wide and ## pixels high
                     (send dc set-font (make-font #:size 16 #:family 'modern
                                                  #:weight 'bold))
                     (send dc set-text-foreground "white")
-                    (send dc draw-text (vector-ref displayLines 0) 10 157)
-                    (send dc draw-text (vector-ref displayLines 1) 10 172)
-                    (send dc draw-text (vector-ref displayLines 2) 10 187)
-                    (send dc draw-text (vector-ref displayLines 3) 10 202)
-                    (send dc draw-text (vector-ref displayLines 4) 10 217)
+                    (send dc draw-text (vector-ref displayLines 0) 10 157) ;display line 0
+                    (send dc draw-text (vector-ref displayLines 1) 10 172) ;display line 1
+                    (send dc draw-text (vector-ref displayLines 2) 10 187) ;display line 2
+                    (send dc draw-text (vector-ref displayLines 3) 10 202) ;display line 3
+                    (send dc draw-text (vector-ref displayLines 4) 10 217) ;display line 4
 
-                    (set! button #f)
-                    (set! potentiometer (send slider get-value))
                     )
                   ]
                  [style '(transparent)]
                  )
   )
 
-;canvas
+;canvas for the bot
 (define bot (new canvas%
                  [parent leftPanel]
                  [paint-callback
                      (λ (c dc)
-                       (send dc clear) ;erase
+                       (send dc clear) ;clear set the standard background, (alt erase)
                        
-                       (send dc draw-bitmap bg_img 0 0)
+                       (send dc draw-bitmap bg_img 0 0) ; draw background with line
                    
-                       
-                       ;bumpers
+                       ;draw bumpers
                        (send dc set-pen "red" 3 'solid)
-                       ;(cond ( (equal? left #f)
+                       ;(cond ( (equal? left #f) ; cond to emulate the bumper touch
                        (send dc draw-arc (- x 20) (- y 20) 40 40 (+ z 0.2) (+ z (/ pi 4)))
                        ;))
                        ;(cond ( (equal? right #f)
@@ -349,9 +447,9 @@
               )
   )
 
-
-
-(define (loop) ;should be in a thread
+;; the refresh function loop
+;; compute positions and update the canvas
+(define (loop)
   ;update status bar
   (send frame set-status-text
         (string-append "IR0: " (format "~a" (point-black ir0))
@@ -366,16 +464,17 @@
                        ))
   (send bot refresh-now)
   (send display refresh-now)
+  (send sliderEmulator refresh-now)
+  (send buttonEmulator refresh-now)
   (position)
   (sleep/yield 0.05)
   (loop)
   )
 
-
+;; loop function for thread
 (define (read-hook)
   (printf "Read thread started ...")
   (loop))
-
 
 ;;racket-main function mapping
 
@@ -385,7 +484,6 @@
     (clearLCD)
     (send frame create-status-line) 
     (send frame show #t)
-    ;(send bg on-paint)
     (set! gui-thread (thread (lambda ()  (read-hook))))
     )
   )
@@ -393,7 +491,7 @@
 ;close the GUI and the thread
 (define close-asip
   (λ ()
-    (when (not (null? gui-thread)) (println "Killing thread .... ") (kill-thread gui-thread))
+    (when (not (null? gui-thread)) (println "Killing gui thread .... ") (kill-thread gui-thread))
     (exit #t)
     (println "closed")
     )
@@ -431,7 +529,7 @@
   )
 
 
-;; Setting both motors at the same time
+;; Set both motors power at the same time
 (define setMotors
   (λ (s1 s2)
     (setMotor 0 s1)
@@ -439,6 +537,7 @@
     )
   )
 
+;; Set the power of the specified motor
 (define setMotor
   (λ (m s)
     (cond ( (equal? m 0) (set! leftWheelPwr s))
@@ -455,12 +554,14 @@
   (λ () (cond ( (> bumpersInterval 0 ) left) (else #f)))
   )
 
+;; Enabling the bumpers
 (define enableBumpers
   (λ (interval)
     (set! bumpersInterval interval)
     )
   )
 
+;; Enabling the infrared sensors
 (define enableIR
   (λ (interval)
     (set! irInterval interval)
@@ -502,7 +603,7 @@
     )
   )
 
-;reset the motor num counter
+;reset motor's num counter
 (define resetCount
   (λ (num)
     (cond ( (equal? num 0) (set! leftCounter 0))
@@ -544,6 +645,6 @@
 (define playTone
   (λ (t d) ;; t in Hz, d in ms is the duration
     (play-sound "beep.wav" #f)
-    (sleep (/ d 1000))
+    (sleep (/ d 1000)) ;maybe not necessary
    )
  )
